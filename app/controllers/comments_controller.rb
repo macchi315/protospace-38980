9ab501a8d9e6238f2fbc5274473bdeb70b46bad9
macchi315 <machidamitsutaka315@gmail.com>
2023-03-02 21:29:08 +0900

    再提出２回目

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,8 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
+      @prototype = @comment.prototype     
+      @comments = @prototype.comments
       render :"prototypes/show"
     end    
   end
@@ -16,3 +18,4 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 end
+
